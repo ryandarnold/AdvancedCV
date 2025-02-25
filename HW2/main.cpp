c5dcaa7597dfa2d0e4bfa215dcd0e625d523e33d
Ryan Arnold <ryan.arnold7916@gmail.com>
2025-02-24 21:08:02 -0500

    got the main.cpp file to open and run a video (although the FPS is much lower than if I run the video in a standard windows media player for some reason

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -3,40 +3,33 @@
 
 using namespace std;
 
-void partA()
+void part2()
 {
-    /*Display any image using OpenCV.
-    Even a small one will do.
-    */
-    cv::Mat image = cv::imread("../../../camel_hehe.jpg"); // Replace with your image path
+    //
+    //step 1: open the video
+    cv::VideoCapture cap;
+    cap.open("../../../Walking_through_Back_Yard.mp4");
+    double fps = cap.get(cv::CAP_PROP_FPS);
+    std::cout << "Frame rate (FPS): " << fps << std::endl;
+    int delay_between_frames = round(1000.0 / fps);
+    cout << "ms delay between frames: " << delay_between_frames << endl;
 
-    // Check if the image was loaded properly
-    if (image.empty()) {
-        std::cerr << "Error: Unable to load image." << std::endl;
+    cv::Mat frame;
+    double Scale = 0.6;
+    while (true)
+    {   //runs the video in a loop
+        cap >> frame;
+        if (frame.empty()) break;
+        cv::Mat resized_frame;
+        cv::resize(frame, resized_frame, cv::Size(), Scale, Scale, cv::INTER_AREA);
+        cv::imshow("example RUN hehe", resized_frame);
+        if (cv::waitKey(delay_between_frames) >= 0) break;
     }
-
-    // Resize image to 50% of its original size
-    cv::Mat resized_image;
-    cv::resize(image, resized_image, cv::Size(), 0.5, 0.5, cv::INTER_AREA);
-
-    // Display the image
-    cv::imshow("Image", resized_image);
-
-    // Wait for a key press and close the window
-    cv::waitKey(0);
 }
 
 int main()
 {
-    cout << "hehe";
-    partA();
-
-    // string main_monopoly_pic = "../../../main_monopoly_picture.jpg";
-    // string scene_image = "../../../SIFT_testing_picture_monopoly.jpg";
-    // string angled_main_monopoly_pic = "../../../angled_main_monopoly_picture.jpg";
-    // cv::Mat warped_current_video_frame;
-    // warped_current_video_frame = testingSIFT(main_monopoly_pic, angled_main_monopoly_pic);
-    // cv::Mat cropped_board = crop_out_background(warped_current_video_frame);
+    part2();
 
     return 0;
 }
