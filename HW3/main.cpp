e61f100349a34c6c38c2e3a3551878646a05d933
Ryan Arnold <ryan.arnold7916@gmail.com>
2025-03-25 21:48:41 -0400

    added the final code i'm turning in for HW3. Also added the background model .jpg image from MOG2, and added the final video mosaic.jpg

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -1,3 +1,14 @@
+/*
+ * This program reads a video file and creates a mosaic of the video frames with the background model
+ * The program uses a background subtractor to find the diver in the video frame
+ * The program then creates a mosaic of the video frames with the background model
+ * The program also keeps track of the parts of the video that have already been occupied by divers
+ * so each diver doesn't 'touch' each other in the mosaic
+ *
+ * ***NOTE***: I used the "diving_video_far__board_477E38120CAF" video (the 2nd video)
+ */
+
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
@@ -12,7 +23,10 @@ void display_video_frame(cv::Mat videoFrameToDisplay, double Scale, string windo
     cv::imshow(window_name, resized_frame);
 }
 
-cv::Mat getBrightestImage(const cv::Mat& img1, const cv::Mat& img2, const cv::Mat& img3) {
+cv::Mat getBrightestImage(const cv::Mat& img1, const cv::Mat& img2, const cv::Mat& img3)
+{
+    /*This function takes in 3 images and returns the brightest image
+     */
     cv::Mat gray1, gray2, gray3;
     cv::cvtColor(img1, gray1, cv::COLOR_BGR2GRAY);
     cv::cvtColor(img2, gray2, cv::COLOR_BGR2GRAY);
@@ -36,44 +50,107 @@ cv::Mat getBrightestImage(const cv::Mat& img1, const cv::Mat& img2, const cv::Ma
     }
 }
 
+
 int main()
 {
-    int number_of_brightest_frames = 4; //4, 6 works well except for beginning;
+    /*
+     * This program reads a video file and creates a mosaic of the video frames with the background model
+    */
+
+    int number_of_brightest_frames = 4; //finding 1 brightest frame out of 4 frames
 
     cv::VideoCapture cap("../../../diving_video.mp4");
-    double fps = cap.get(cv::CAP_PROP_FPS);
-    // double framecount = cap.get(cv::CAP_PROP_FRAME_COUNT);
-    int totalFrameCount = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_COUNT));
+    int totalFrameCount = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_COUNT)); //finds the total number of frames in the video
 
     //ignore the last 'x' number of frames if they're not a multiple of 'number_of_brightest_frames'
     int totalFramesToProcess = totalFrameCount - (totalFrameCount % number_of_brightest_frames);
     cout << "Total frames: " << totalFrameCount << endl;
-    cout << "Frame rate (FPS): " << fps << endl;
 
+    //create a background subtractor object
+    cv::Ptr<cv::BackgroundSubtractor> background_subtractor = cv::createBackgroundSubtractorMOG2();
 
-    cv::Mat currentFrame;
-    std::vector<cv::Mat> buffer;
+    //load the background model .jpg image
+    cv::Mat backgroundModel = cv::imread("../background_Diving_model.jpg");
+
+    cv::Mat currentFrame; //current frame from the video
+    cv::Mat videoMosaic = backgroundModel.clone(); //initialize the video mosaic with the background model
+
+    //need to track the parts of the video that have already been occupied by divers
+    // so each diver doesn't 'touch' each other in the mosaic
+    cv::Mat occupiedArea = cv::Mat::zeros(backgroundModel.size(), CV_8UC1);;
+
+    std::vector<cv::Mat> buffer; //buffer to store the last 'number_of_brightest_frames' frames
     cv::Mat brightestFrame;
-    int frameCount = 1;
+    int frameCount = 1; //frame count to know when to process the brightest frame
+
     while (true) {
         cap >> currentFrame; // grab new video frame
-        buffer.push_back(currentFrame); //add the current frame to the buffer to keep track of brightest three frames
+        if (currentFrame.empty()) {
+            break; // exit if video is over
+        }
+
+        buffer.push_back(currentFrame); //add the current frame to the buffer to
+        //keep track of brightest number_of_brightest_frames
 
         if ((frameCount % number_of_brightest_frames == 0) && (frameCount < totalFramesToProcess))
         {
-            //now to check the brightest frame in the last three frames
+            //frameCount % number_of_brightest_frames == 0 means we have gone 'number_of_brightest_frames' frames
+            //frameCount < totalFramesToProcess means we only go up to a multiple
+            //of number_of_brightest_frames (i was getting getting errors if i don't include this)
+
+            //now to check the brightest frame in the last "number_of_brightest_frames"
             cv::Mat brightestFrameInBuffer = getBrightestImage(buffer[0], buffer[1], buffer[2]);
-            //
+
+            cv::Mat fgMask; //everything white is the foreground mask
+            background_subtractor->apply(brightestFrameInBuffer, fgMask); //apply background subtraction
+
+            //from here down, the fgMask is the foreground (white), and everything else is black
+
+            // Clean the mask to get better foreground object of the diver
+            cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(7, 7));
+            cv::erode(fgMask, fgMask, kernel, cv::Point(-1, -1), 1);
+            cv::dilate(fgMask, fgMask, kernel, cv::Point(-1, -1), 6);
+
+            //threshold because the mask is not binary, because of the erode and dilate kernel
+            //i.e. parts of the mask become gray because of the erode and dilate
+            cv::threshold(fgMask, fgMask, 200, 255, cv::THRESH_BINARY);
+
+            // mask out water because it messes with below 'diver area' and 'overlap area' calculations
+            int waterlineY = 1000; // adjust for different video heights
+            cv::Rect waterLine(0, waterlineY, fgMask.cols, fgMask.rows - waterlineY);
+            fgMask(waterLine) = 0; // set water area to black always
+
+            cv::Mat overlap; //finds the overlap between the occupied area and the fgMask
+            cv::bitwise_and(occupiedArea, fgMask, overlap);
+
+            //needs to find the area of the diver and the overlap area
+            int diverArea = cv::countNonZero(fgMask);
+            int overlapArea = cv::countNonZero(overlap);
+
+            //only add the diver to the mosaic if the diver area is greater than 10000
+            //and the overlap area is 0 (i.e. the divers aren't touching in mid-air)
+            if (diverArea > 1000 && overlapArea == 0) { //anything less than 14000ish works to get all divers
+                //copies brightestFrameInBuffer, with the fgMask as the mask, to the videoMosaic
+                brightestFrameInBuffer.copyTo(videoMosaic, fgMask);
+
+                // adds the current 'fgMask' to the 'occupiedArea' so the next diver doesn't touch the current diver
+                cv::bitwise_or(occupiedArea, fgMask, occupiedArea);
+            }
+
             display_video_frame(brightestFrameInBuffer, 0.5, "Diving Video");
+            display_video_frame(fgMask, 0.5, "Current Foreground Mask");
+
             if (int key = cv::waitKey(33); key >= 0) { break;} // displays at 30FPS
             buffer.clear();
         }
 
         frameCount++;
-        // display_video_frame(currentFrame, 0.5, "Diving Video"); //displays normal video
-        // if (int key = cv::waitKey(33); key >= 0) { break;} // displays at 30FPS
     }
 
+    cv::destroyAllWindows();
+    display_video_frame(videoMosaic, 0.5, "Final Diver Mosaic");
+    // cv::imwrite("../Final Diver Mosaic.jpg", videoMosaic); // Save to disk
+    cv::waitKey(0);
     return 0;
 }
 
